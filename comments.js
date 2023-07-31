e026124bbe99eb9249ced84952ef1f6dbd105326
Gouse <gousemohammad73@gmail.com>
2023-07-31 16:38:52 +0530

    Copilot third  commit

--- comments.js
+++ comments.js
@@ -2,6 +2,11 @@
 const http = require('http');
 const port = 3000;
 const server = http.createServer((req, res) => {
-    res.end('Hello World!');
-    }
-);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Hello World</h1>");
+    res.end();
+});
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
